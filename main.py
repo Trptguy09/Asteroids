5851f790b9fd4142a1c639cef2de513642feaf84
Trptguy09 <nervouspuddin@gmail.com>
2025-09-09 14:32:59 -0400

    B: progress  2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import *
 from constants import *
 
 def main():
@@ -8,19 +9,20 @@ def main():
     black = (0,0,0)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("ASTEROIDS")
+    clock = pygame.time.Clock()
+    dt = 0
+    Player.__init__(x,y)
+    x = SCREEN_WIDTH/2
+    y = SCREEN_HEIGHT/2
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(black, rect=None, special_flags=0)
+        Player.draw(screen)
         pygame.display.flip()
-
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
-
+        clock.tick(60)
 
 if __name__ == "__main__":
     main()
